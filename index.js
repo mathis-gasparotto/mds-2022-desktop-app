59a942b5357c0ded4c65c19cffe5cc197b07e9c3
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-06-08 17:42:27 +0200

    :construction: Work with data

--- index.js
+++ index.js
@@ -1,14 +1,83 @@
 addEventListener('DOMContentLoaded', () => {
   event.preventDefault()
+
+  let todos = [
+    {
+      title: 'Test',
+      content: '',
+      datetime: new Date(),
+      important: false,
+      completed: false
+    },
+    {
+      title: 'Test',
+      content: '',
+      datetime: new Date(),
+      important: false,
+      completed: false
+    },
+    {
+      title: 'Test',
+      content: '',
+      datetime: new Date(),
+      important: false,
+      completed: false
+    },
+    {
+      title: 'Test',
+      content: '',
+      datetime: new Date(),
+      important: false,
+      completed: false
+    },
+    {
+      title: 'Test',
+      content: '',
+      datetime: new Date(),
+      important: false,
+      completed: false
+    }
+  ]
+
+  const todoList = document.getElementById('todos')
+
+  todos.forEach(todo => {
+    printTodo(todo)
+  })
+
   const form = document.getElementById('create-todo-form')
   form.addEventListener('submit', (event) => {
     event.preventDefault()
-    const todo = document.createElement('li')
-    const todoList = document.getElementById('todos')
-    todo.innerText = event.target[0].value
-    todo.classList.add('todo')
+    addTodo(event.target[0].value, event.target[1].value, event.target[2].value, event.target[3].checked)
     event.target[0].value = ''
-    todoList.appendChild(todo)
+    event.target[1].value = ''
+    event.target[2].value = ''
+    event.target[3].checked = false
   })
+
+  function addTodo(title, content, datetime, important, completed = false) {
+    const todo = {
+      title,
+      content,
+      datetime,
+      important,
+      completed
+    }
+    todos.push(todo)
+    printTodo(todo)
+  }
+
+  function printTodo(todoObject) {
+    const todo = document.createElement('li')
+    const delBtn = document.createElement('button')
+    delBtn.classList.add('btn')
+    delBtn.classList.add('btn-danger')
+    todo.innerText = todoObject.title
+    todo.appendChild(delBtn)
+    todo.classList.add('todo')
+    if (todoObject.completed) todo.classList.add('completed')
+    if (todoObject.important) todo.classList.add('important')
+    todoList.appendChild(todo)
+  }
   
 })
